949c303bb461bf9864ac6ded40b1d83ceb023d41
你的英文名 <>
2018-02-05 20:03:57 +0800

    ..

---
+++ while.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*{
+    int sum = 0, val = 1;
+    while(val <= 10){
+        sum += val;
+        ++val;
+    }
+    int sum2 = 0,length = 10;
+    int val2 = 2;
+    for (size_t i = 0; i < length; i++)
+    {
+        sum2 += val2;
+        ++val2;
+    }
+    cout << "Sum of 1 to 10 inclusive is " << sum <<"ss"<<sum2<< endl;
+    cout << "sum2" << sum2 << endl;
+
+    return 0;
+}
+
+{
+    int sum = 0, value = 0;
+    while(std::cin>>value)
+        sum += value;
+    std::cout << "Sum is: " << sum << std ::endl;
+    return 0;
+}
+
+    int currVal = 0,val = 0;
+       if (cin >> currVal){
+           int cnt = 1;
+           while (cin >> val){
+               if (val == currVal)
+                   ++cnt;
+                else
+                {
+                    cout << currVal << " occurs "
+                          << cnt << " times " << endl;
+                    currVal = val;
+                    cnt = 1;
+                }
+           }
+
+           cout << currVal << "occurs"
+                << cnt << "times" << endl;
+       }
+
+    }
+
+    unsigned u = 10,j = 9,k = -1,p = -2;
+    int i = -42,l = 1;
+    cout << i + u << endl;
+    cout << u - j << endl;
+    cout << j - u <<" "<< k << endl;
+    cout << k - l << endl;
+    cout << p << endl;
+
+    string book("try try try");
+    cout << book << endl;
+    int i = {3};
+    int i;
+    i = 10;
+    cout << i << endl;
+    i = 100;
+    cout << i << endl;
+    i = 100;
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    for (size_t i = 0; i < 8; i++)
+    {
+        a++;
+        b--;
+    }
+    cout << a << b << endl;
+    int i = 0, a, b;
+    a = i++;
+    cout << i << endl;
+    b = ++i;
+    cout << i << endl;
+    cout << a << b << endl;
+
+    for (size_t i = 0; i < length; i++)
+    {
+
+    }
+    int ival = 1024;
+    int &refval = ival;
+
+    int i, &ri = i;
+    i = 5;
+    ri = 10;
+   // cout << i << " " << ri << endl;
+    int *d = &refval;
+  //  cout << *d;
+    int j = 43;
+    int &r = j;
+    int *p = &j;
+    p = &j;
+    cout << &p;
+    int &re = *p;
+    cout << re;
+    cout << *p;
+    *p = i;
+   // int &re = *p;
+    cout << j;
+    cout << *p;
+    cout << re;*/
+   // const int i = 8,*p = &i;
+    int *p;
+    auto *&i = p;
+}
